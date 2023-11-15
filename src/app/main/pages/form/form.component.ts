892ffb64872b3c349a2605d540ea7334d14a9318
Bautista Madruga <bmadruga@wilab.io>
2023-11-15 18:54:37 -0300

    added some stuff

--- src/app/main/pages/form/form.component.ts
+++ src/app/main/pages/form/form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ITemas } from '../../api/models/i-temas';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ITipos } from '../../api/models/i-tipos';
+import { TemasResourceService } from '../../api/resources/temas-resource.service';
 
 @Component({
   selector: 'app-form',
@@ -17,7 +18,7 @@ export class FormComponent {
 
 
   constructor(private _fb: FormBuilder,
-    // private _personaService: PersonaResourceService,
+    private _temasService: TemasResourceService,
     private _route: ActivatedRoute,
     private router: Router) {
 
@@ -31,6 +32,8 @@ export class FormComponent {
   private initForm(): void {
     this.form = this._fb.group({
       tiposServicios: [this.tipos],
+      temas: [''],
+      sugerencia: ['']
     })
   }
 
@@ -43,16 +46,45 @@ export class FormComponent {
     this.reset();
   }
 
-
   listarDatosResolver(): void {
     let tipo: ITipos = {
       codTipoServicio: "XX",
-      tipoServicio: "Selecciona un tipo de servicio"
+      tipoServicio: $localize`Selecciona un tipo de servicio`
     }
     this.tipos.push(tipo)
     console.log(tipo.codTipoServicio)
     this._route.data.subscribe((data) => {
       this.tipos.push(...data["tipos"])
+    })
+  }
+
+  onChange(value: any) { 
+    let codTipoServicio: string = value.target.value;
+    
+    this.temas = [];
+    this.reset();
+
+    if (codTipoServicio === 'XX') {
+      return;
+    }
+
+    this._temasService.getTemas({codTipoServicio: codTipoServicio }).subscribe ({
+      next: ( temas: ITemas[] ) => {
+
+        let tema: ITemas = {
+          codTipoServicio: codTipoServicio,
+          tema: $localize`Seleccione un tema`,
+          nroTema: "00"
+        }
+
+        this.temas.push(tema);
+        this.temas.push(...temas);
+        this.form.patchValue({temas: this.temas})
+
+      },
+      error: ( err ) => {
+        throw err;
+      }
     })
   }
 }
